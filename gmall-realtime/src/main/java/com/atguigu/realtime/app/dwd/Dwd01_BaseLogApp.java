ce562629e50e7fe151a0de859cd1468a8ed1b297
liqingxin <liqxgut@163.com>
2022-11-13 00:41:39 +0800

    【Dwd01_BaseLogApp】消费topic_log主题的数据并打印输出

---
+++ gmall-realtime/src/main/java/com/atguigu/realtime/app/dwd/Dwd01_BaseLogApp.java
@@ -0,0 +1,60 @@
+package com.atguigu.realtime.app.dwd;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.realtime.util.KafkaUtil;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author liqingxin
+ * 2022/11/13 0:35
+ */
+public class Dwd01_BaseLogApp {
+    public static void main(String[] args) throws Exception {
+        // TODO 1. 初始化环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(4);
+
+        // TODO 2. 启用状态后端
+        env.enableCheckpointing(3000L, CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setCheckpointTimeout(60 * 1000L);
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000L);
+        env.getCheckpointConfig().enableExternalizedCheckpoints(
+                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION
+        );
+        env.setRestartStrategy(RestartStrategies
+                .failureRateRestart(10,
+                        Time.of(3L, TimeUnit.DAYS),
+                        Time.of(1L, TimeUnit.MINUTES)));
+        env.setStateBackend(new HashMapStateBackend());
+        env.getCheckpointConfig().setCheckpointStorage("hdfs://hadoop102:8020/gmall/ck");
+        System.setProperty("HADOOP_USER_NAME", "atguigu");
+
+        // TODO 3. 从 Kafka 读取主流数据
+        String topic = "topic_log";
+        String groupId = "base_log_consumer";
+        DataStreamSource<String> source = env.addSource(KafkaUtil.getKafkaConsumer(topic, groupId));
+        source.print("source");
+
+        env.execute();
+    }
+}
--- gmall-realtime/src/main/java/com/atguigu/realtime/util/KafkaUtil.java
+++ gmall-realtime/src/main/java/com/atguigu/realtime/util/KafkaUtil.java
@@ -1,10 +1,15 @@
 package com.atguigu.realtime.util;
 
+import edu.umd.cs.findbugs.annotations.Nullable;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.streaming.connectors.kafka.KafkaDeserializationSchema;
+import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
 
@@ -43,4 +48,22 @@ public class KafkaUtil {
                 }, prop);
         return consumer;
     }
+
+    public static FlinkKafkaProducer<String> getKafkaProducer(String topic) {
+
+        Properties prop = new Properties();
+        prop.setProperty("bootstrap.servers", BOOTSTRAP_SERVERS);
+        prop.setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 60 * 15 * 1000 + "");
+
+        FlinkKafkaProducer<String> producer = new FlinkKafkaProducer<String>(DEFAULT_TOPIC, new KafkaSerializationSchema<String>() {
+
+            @Override
+            public ProducerRecord<byte[], byte[]> serialize(String jsonStr, @Nullable Long timestamp) {
+                return new ProducerRecord<byte[], byte[]>(topic, jsonStr.getBytes());
+            }
+        }, prop, FlinkKafkaProducer.Semantic.EXACTLY_ONCE);
+
+        return producer;
+    }
+
 }
